a4c58707218c565ff51c9fb7d33e4b223a9cc291
Diego Lopez <lopexdiego5@gmail.com>
2020-06-02 15:48:10 -0500

    infopokemon creado. input de busqueda creado

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import SearchPokemons from '../components/SearchPokemons';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hola mundo</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemons: []
+    }
+    this._getInputValue = this._getInputValue.bind(this);
+  }
+
+  componentDidMount() {
+
+  }
+
+  _getInputValue(e) {
+    console.log(e.target.value);
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <SearchPokemons onKeyUp={this._getInputValue} />
+        asas
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/InfoPokemon.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class InfoPokemon extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <div>
+                <h1>InfoPokemon</h1>
+            </div>
+        );
+    }
+}
+
+export default InfoPokemon;

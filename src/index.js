d2c5a1f5a349120e7079bf83be5e4e4e6934ea8a
ctatianap <taty.pl_0906@hotmail.com>
2020-06-03 20:57:51 -0500

    React router transition

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { AnimatedSwitch } from 'react-router-transition';
 
 import App from './pages/App';
 import InfoPokemon from './pages/InfoPokemon'
@@ -10,8 +11,15 @@ import './scss/style.scss'
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+    <AnimatedSwitch
+      atEnter={{ opacity: 0 }}
+      atLeave={{ opacity: 0 }}
+      atActive={{ opacity: 1 }}
+      className="switch-wrapper"
+    >
       <Route exact path="/" component={App} />
       <Route exact path="/infopokemon/:name" component={InfoPokemon} />
+      </AnimatedSwitch>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
